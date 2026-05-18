b918326c1cb2c316db96996fd64ab559fd05efbe
esraaaldamiry1996 <esraaaldamiry1996@gmail.com>
2026-05-18 12:37:15 +0300

    Updating the code and adding new features

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,48 @@
 import 'package:flutter/material.dart';
-import 'dart:math' as math;
+import 'package:flutter/services.dart';
 
 void main() {
-  runApp(const MyApp());
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]).then((_) {
+    runApp(const MyApp());
+  });
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'تطبيق بصمة ',
+      title: 'تطبيق بصمة',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.purple),
       ),
-      home: Welcome_screen(),
+      home: const WelcomeScreen(),
     );
   }
 }
 
-class Welcome_screen extends StatefulWidget {
-  const Welcome_screen({super.key});
+class WelcomeScreen extends StatefulWidget {
+  const WelcomeScreen({super.key});
 
   @override
-  State<Welcome_screen> createState() => _Welcome_screenState();
+  State<WelcomeScreen> createState() => _WelcomeScreenState();
 }
 
-class _Welcome_screenState extends State<Welcome_screen> {
-  bool _isVisible = false;
+class _WelcomeScreenState extends State<WelcomeScreen> {
+  
   bool _isButtonVisible = false;
+
   @override
   void initState() {
     super.initState();
-    Future.delayed(Duration(milliseconds: 500), () {
-      setState(() {
-        _isVisible = true;
-      });
-    });
-    Future.delayed(Duration(milliseconds: 1500), () {
+
+    Future.delayed(const Duration(milliseconds: 1500), () {
       setState(() {
         _isButtonVisible = true;
       });
@@ -50,10 +55,11 @@ class _Welcome_screenState extends State<Welcome_screen> {
       body: Container(
         width: double.infinity,
         height: double.infinity,
+
         decoration: const BoxDecoration(
           gradient: LinearGradient(
-            begin: AlignmentGeometry.topLeft,
-            end: AlignmentGeometry.bottomRight,
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
             colors: [
               Color.fromARGB(255, 192, 226, 246),
               Color.fromARGB(255, 208, 246, 227),
@@ -67,19 +73,16 @@ class _Welcome_screenState extends State<Welcome_screen> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             TweenAnimationBuilder(
-              tween: Tween<double>(begin: 0, end: 1),
-              duration: Duration(seconds: 2),
+              tween: Tween<double>(begin: 0.0, end: 1.0),
+              duration: const Duration(milliseconds: 1500),
               curve: Curves.elasticOut,
               builder: (context, double value, child) {
                 return Transform.scale(
                   scale: value,
                   alignment: Alignment.center,
-                  
-                    
                   child: child,
                 );
               },
-
               child: Column(
                 children: [
                   Image.asset(
@@ -88,8 +91,9 @@ class _Welcome_screenState extends State<Welcome_screen> {
                     height: 250,
                     fit: BoxFit.contain,
                   ),
-                  Text(
-                    'لكل طفل حكاية ',
+
+                  const Text(
+                    'لكل طفل حكاية',
                     style: TextStyle(
                       fontSize: 28,
                       fontWeight: FontWeight.bold,
@@ -99,21 +103,25 @@ class _Welcome_screenState extends State<Welcome_screen> {
                 ],
               ),
             ),
-            SizedBox(height: 30),
+
+            const SizedBox(height: 30),
+
             AnimatedOpacity(
               opacity: _isButtonVisible ? 1.0 : 0.0,
-              duration: Duration(seconds: 1),
+              duration: const Duration(seconds: 1),
               child: GestureDetector(
                 onTap: () {},
                 child: Container(
                   width: 250,
                   height: 75,
-
-                  padding: EdgeInsets.symmetric(horizontal: 60, vertical: 12),
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 60,
+                    vertical: 12,
+                  ),
                   decoration: BoxDecoration(
-                    gradient: LinearGradient(
-                      begin: AlignmentGeometry.centerLeft,
-                      end: AlignmentGeometry.centerRight,
+                    gradient: const LinearGradient(
+                      begin: Alignment.centerLeft,
+                      end: Alignment.centerRight,
                       colors: [Color(0xFF70A5A5), Color(0xFF7CB174)],
                     ),
                     borderRadius: BorderRadius.circular(30),
@@ -125,9 +133,9 @@ class _Welcome_screenState extends State<Welcome_screen> {
                       ),
                     ],
                   ),
-                  child: Center(
+                  child: const Center(
                     child: Text(
-                      'تسجيل الدخول ',
+                      'تسجيل الدخول',
                       style: TextStyle(
                         color: Colors.white,
                         fontSize: 22,
